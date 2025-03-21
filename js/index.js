c30e1b9f1bd04aee14a5fe23650c7a8ec46f52e7
juuliaaj <juliajardim765@gmail.com>
2025-03-21 00:42:46 -0300

    correção navbar celulares

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 const body = document.querySelector("body");
 const header = document.querySelector(".custom-header");
+const navbarToggler = document.querySelector(".navbar-toggler") 
+
+let navAberta = false;
+let scrollPosition = 0;
 
 body.addEventListener("scroll", function() {
-    let scrollPosition = body.scrollTop;
+    scrollPosition = body.scrollTop;
     
-    if (scrollPosition > 0) {
+    fundoNavbar();
+});
+
+navbarToggler.addEventListener("click", function() {
+    navAberta = !navAberta;
+
+    fundoNavbar();
+});
+
+function fundoNavbar() {
+    if ((scrollPosition > 0 || navAberta) && !header.classList.contains("visible")) {
         header.classList.add("visible");
-    } else {
+    } else if (scrollPosition === 0 && !navAberta && header.classList.contains("visible")) {
         header.classList.remove("visible");
     }
-});
+}
